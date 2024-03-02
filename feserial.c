d6d55494bcf607edccc5fad9f962ff0d50b0681d
Nikola Vlaskalin <nikola.vlaskalin00@gmail.com>
2024-03-02 02:03:44 +0100

    Implemented get_freq from device tree. To be continued.

--- feserial.c
+++ feserial.c
@@ -7,6 +7,10 @@
 #include <linux/resource.h>
 #include <linux/pm_runtime.h>
 #include <asm/io.h>
+#include <linux/clk.h>
+// Offset values defined in -> bus.h and serial.h
+#include <linux/amba/bus.h>
+#include <linux/amba/serial.h>
 
 // Driver structure
 struct uart_dev {
@@ -37,6 +41,7 @@ static int feserial_probe(struct platform_device *pdev)
 	pr_info("Called feserial_probe\n");
     struct uart_dev *dev;
     struct resource *res;
+    struct clk *uart_clk;
     int ret;
 
     // Allocate memory
@@ -50,18 +55,36 @@ static int feserial_probe(struct platform_device *pdev)
     res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
     if (!res) {
         dev_err(&pdev->dev, "Can not get base address!\n");
+        return -EBUSY;
     };
+
+    // TODO Check if it prints.
+    pr_info("Base address: %pa.\n", res->start);
 
     // Remap resources 
     dev->regs = devm_ioremap_resource(&pdev->dev, res);
     if (!dev->regs) {
         dev_err(&pdev->dev, "Can not remap registers!\n");
+        return -ENOMEM;
     };
 
     // Set power management
     pm_runtime_enable(&pdev->dev);
     pm_runtime_get_sync(&pdev->dev);
 
+    // Get frequency from device tree
+    uart_clk = devm_clk_get(&pdev->dev, NULL);
+
+    if (!uart_clk) {
+        dev_err(&pdev->dev, "Could not get uart0 clock.\n");
+        return -EBUSY;
+    }
+
+    ret = clk_prepare_enable(uart_clk);
+    if (ret) {
+        dev_err(&pdev->dev, "Unable to enable uart0 clock!\n");
+        return ret;
+    }
 
 
 	return 0;
